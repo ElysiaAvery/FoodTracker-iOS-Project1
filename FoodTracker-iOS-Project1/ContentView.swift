0d0a48bbfaa79120f993196e5525b2b48d3d72d8
ElysiaAvery <elysia.avery@gmail.com>
2020-10-11 11:08:31 -0700

    Initial Commit

---
+++ FoodTracker-iOS-Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker-iOS-Project1
+//
+//  Created by Avery Nason on 10/11/20.
+//  Copyright © 2020 Avery Nason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
